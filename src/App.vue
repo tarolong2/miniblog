4ecf0e28cb9857406801cda7471175317b49f6da
tarolong2 <tarolong2@gmail.com>
2022-08-25 11:47:41 +0900

    html 초기화, css 초기화, 컴포넌트 배치

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,58 @@
 <template>
-  
+  <div class="wrap">
+    <BlogHeader/>
+    <BlogInput/>
+    <BlogList/>
+    <BlogFooter/>
+  </div>
 </template>
 
 <script>
+import BlogHeader from '@/components/BlogHeader.vue'
+import BlogInput from '@/components/BlogInput.vue'
+import BlogList from '@/components/BlogList.vue'
+import BlogFooter from '@/components/BlogFooter.vue'
 export default {
-
+  components: {
+    BlogHeader,
+    BlogInput,
+    BlogList,
+    BlogFooter
+  }
 }
 </script>
 
 <style>
+@charset 'utf-8';
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800;900&family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;  
+}
+ul, li {
+  list-style: none;
+}
+img {
+  vertical-align: middle;
+  border: 0;
+}
+a {
+  color: #333;
+  text-decoration: none;
+}
+html {
+  font-size: 16px;
+  overflow-x:hidden;
+}
+body {
+  font-family: 'Montserrat', 'Noto Sans KR', sans-serif;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.2;
+  letter-spacing: -0.64;
+  color: #000;
+}
+
 
 </style>
